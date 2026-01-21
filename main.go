8ad02e154b7b42feb03b5bf030be69fb422889a2
MD Al Mamun Galib <108384550+mdalmamungalib@users.noreply.github.com>
2026-01-21 12:24:25 +0600

    update some file

--- main.go
+++ main.go
@@ -1,29 +1,59 @@
 package main
-
 import (
-    "log"
-    "os"
-
-    "ecommerce/database"
-    "ecommerce/router"
-    
-    "github.com/joho/godotenv"
+	"example.com/method"
 )
 
-func init() {
-    if _, err := os.Stat(".env"); err == nil {
-        err = godotenv.Load(".env")
-        if err != nil {
-            log.Fatal("Error loading .env config file")
-        }
-        log.Println("Successfully loaded the config file")
-    } else {
-        log.Println(".env file not found, skipping load")
-    }
-}
+// var (
+// 	a = 10
+// 	b = 20
+// )
 
-func main() {
-    database.ConnectDb()
+// func main() {
+// 	method.Add(20, 30)
+// }
 
-    router.ClientRoutes()
-}
+// func welcomeSection() {
+// 	fmt.Println("Welcome to the application")
+// }
+
+// func getName() string {
+// 	var name string
+// 	fmt.Println("Please enter your name ?")
+// 	fmt.Scanln(&name)
+// 	return name
+// }
+
+// func getNumbers() (int, int) {
+// 	var num1 int
+// 	var num2 int
+
+// 	fmt.Println("Enter your first number...")
+// 	fmt.Scanln(&num1)
+// 	fmt.Println("Enter your second number...")
+// 	fmt.Scanln(&num2)
+// 	return num1, num2
+// }
+
+// func addNumber(num1 int, num2 int) int {
+// 	sum := num1 + num2
+// 	return sum
+// }
+
+// func showDetails(name string, sum int) {
+// 	fmt.Println("Hello", name)
+// 	fmt.Println("Summation =", sum)
+// }
+
+// func lastMessage() {
+// 	fmt.Println("Thank you for using the application")
+// 	fmt.Println("Hav a good day.")
+// }
+
+// func main() {
+// 	welcomeSection()
+// 	name := getName()
+// 	num1, num2 := getNumbers()
+// 	sum := addNumber(num1, num2)
+// 	showDetails(name, sum)
+// 	lastMessage()
+// }
